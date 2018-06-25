c83577fba61749578d162732d89e0d0158f0bb27
hillin <hillinsilence@gmail.com>
2018-06-25 12:44:44 +0800

    add cancellation support

--- Procedure.cs
+++ Procedure.cs
@@ -1,66 +1,65 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Hillinworks.WorkflowFramework
 {
-	public abstract class Procedure
-	{
-		public event EventHandler Completed;
+    public abstract class Procedure
+    {
+        public bool IsCompleted { get; private set; }
 
-		public bool IsCompleted { get; private set; }
+        public Workflow Workflow { get; private set; }
+        public Procedure Predecessor { get; private set; }
+        protected internal CancellationToken CancellationToken => this.Workflow.CancellationTokenSource.Token;
+        public event EventHandler Completed;
 
-		public Workflow Workflow { get; private set; }
-		public Procedure Predecessor { get; private set; }
+        internal virtual void InternalInitialize(Workflow workflow, Procedure predecessor)
+        {
+            this.Workflow = workflow;
+            this.Predecessor = predecessor;
+            this.CancellationToken.Register(this.OnCancelled);
+        }
 
-		internal virtual void InternalInitialize(Workflow workflow, Procedure predecessor)
-		{
-			this.Workflow = workflow;
-			this.Predecessor = predecessor;
-		}
 
-		protected internal virtual void Initialize()
-		{
+        protected internal virtual void Initialize()
+        {
+        }
 
-		}
+        protected internal virtual void Start()
+        {
+        }
 
-		protected internal virtual void Start()
-		{
+        /// <summary>
+        ///     Invoke <see cref="IProcedureInput{TInput}.ProcessInput(TInput)" /> on this procedure with reflection, asserting
+        ///     this procedure implements <see cref="IProcedureInput{TInput}" />.
+        /// </summary>
+        internal void InvokeProcessInput(object input)
+        {
+            var processInputMethod = typeof(IProcedureInput<>).MakeGenericType(input.GetType())
+                .GetMethod(nameof(IProcedureInput<object>.ProcessInput));
 
-		}
+            Debug.Assert(processInputMethod != null);
 
-		/// <summary>
-		/// Invoke <see cref="IProcedureInput{TInput}.ProcessInput(TInput)"/> on this procedure with reflection, asserting
-		/// this procedure implements <see cref="IProcedureInput{TInput}"/>.
-		/// </summary>
-		internal void InvokeProcessInput(object input)
-		{
-			var processInputMethod = typeof(IProcedureInput<>).MakeGenericType(input.GetType())
-				.GetMethod(nameof(IProcedureInput<object>.ProcessInput));
+            processInputMethod.Invoke(this, new[] {input});
+        }
 
-			Debug.Assert(processInputMethod != null);
+        protected virtual void OnCancelled()
+        {
+        }
 
-			processInputMethod.Invoke(this, new[] { input });
+        protected virtual void OnCompleted()
+        {
+            this.IsCompleted = true;
+            this.Completed?.Invoke(this, EventArgs.Empty);
+        }
 
-		}
-		
-		protected virtual void OnCompleted()
-		{
-			this.IsCompleted = true;
-			this.Completed?.Invoke(this, EventArgs.Empty);
-		}
+        protected void SubscribeCompleted(Procedure predecessor)
+        {
+            predecessor.Completed += (sender, e) => { this.OnPredessorCompleted(); };
+        }
 
-		protected void SubscribeCompleted(Procedure predecessor)
-		{
-			predecessor.Completed += (sender, e) =>
-			  {
-				  this.OnPredessorCompleted();
-			  };
-		}
-
-		protected virtual void OnPredessorCompleted()
-		{
-		}
-
-	}
-
+        protected virtual void OnPredessorCompleted()
+        {
+        }
+    }
 }
--- Workflow.cs
+++ Workflow.cs
@@ -3,40 +3,58 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Hillinworks.WorkflowFramework
 {
-	public abstract class Workflow
-	{
-		public WorkflowStatus Status { get; private set; } = WorkflowStatus.NotStarted;
+    public abstract class Workflow
+    {
+        public WorkflowStatus Status { get; private set; } = WorkflowStatus.NotStarted;
 
-		public void Start()
-		{
-			if (this.Status != WorkflowStatus.NotStarted)
-			{
-				throw new InvalidOperationException(
-					$"cannot start a workflow which is not in {nameof(WorkflowStatus.NotStarted)} state");
-			}
+        internal CancellationTokenSource CancellationTokenSource { get; private set; }
 
-			var builder = new WorkflowBuilder.Initiator(this);
-			this.Build(builder);
+        public void Start()
+        {
+            if (this.Status != WorkflowStatus.NotStarted)
+            {
+                throw new InvalidOperationException(
+                    $"cannot start a workflow which is not in {nameof(WorkflowStatus.NotStarted)} state");
+            }
 
-			builder.ProcedureChain.Close();
-			
-			var procedures = builder.ProcedureChain.Initialize();
+            var builder = new WorkflowBuilder.Initiator(this);
+            this.Build(builder);
 
-			procedures[procedures.Length - 1].Completed += (sender, e) =>
-			{
-				Debug.Assert(this.Status == WorkflowStatus.Running);
-				this.Status = WorkflowStatus.Ended;
-			};
+            builder.ProcedureChain.Close();
 
-			this.Status = WorkflowStatus.Running;
-			
-			procedures[0].Start();
-		}
+            var procedures = builder.ProcedureChain.Initialize();
 
-		protected abstract void Build(IWorkflowInitiator builder);
-	}
+            Debug.Assert(procedures.Length > 0);
+
+            this.CancellationTokenSource = new CancellationTokenSource();
+
+            procedures[procedures.Length - 1].Completed += (sender, e) =>
+            {
+                Debug.Assert(this.Status == WorkflowStatus.Running);
+                this.Status = WorkflowStatus.Ended;
+            };
+
+            this.Status = WorkflowStatus.Running;
+
+            procedures[0].Start();
+        }
+
+        public void Cancel(bool throwOnFirstException = true)
+        {
+            if (this.Status != WorkflowStatus.Running)
+            {
+                throw new InvalidOperationException(
+                   $"cannot cancel a workflow which is not in {nameof(WorkflowStatus.Running)} state");
+            }
+
+            this.CancellationTokenSource.Cancel(throwOnFirstException);
+        }
+
+        protected abstract void Build(IWorkflowInitiator builder);
+    }
 }
